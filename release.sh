88503a6cd35c403a7cf3e539822d0f412dfb183e
notro <notro@tronnes.org>
2014-03-18 18:40:20 +0100

    release.sh: extra.log should not be appended to

--- release.sh
+++ release.sh
@@ -15,7 +15,7 @@ WORKDIR=${WORKDIR} python -u ${DIR}/task.py $1 init 2>&1 | tee ${WORKDIR}/init.l
 WORKDIR=${WORKDIR} python -u ${DIR}/task.py $1 config 2>&1 | tee ${WORKDIR}/config.log
 WORKDIR=${WORKDIR} python -u ${DIR}/task.py $1 build 2>&1 | tee ${WORKDIR}/build.log
 WORKDIR=${WORKDIR} python -u ${DIR}/task.py $1 modules_install 2>&1 | tee ${WORKDIR}/modules_install.log
-WORKDIR=${WORKDIR} python -u ${DIR}/task.py $1 extra 2>&1 | tee -a ${WORKDIR}/extra.log
+WORKDIR=${WORKDIR} python -u ${DIR}/task.py $1 extra 2>&1 | tee ${WORKDIR}/extra.log
 WORKDIR=${WORKDIR} python -u ${DIR}/task.py $1 readme update_repo
 cp ${WORKDIR}/{init.log,config.log,build.log,modules_install.log,extra.log} ${REPODIR}/extra/
 WORKDIR=${WORKDIR} python -u ${DIR}/task.py $1 commit
