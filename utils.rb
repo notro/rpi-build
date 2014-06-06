a53cc7f6ab7a265c3d70ad881dc84148acca0904
notro <notro@tronnes.org>
2014-06-06 21:43:48 +0200

    allow CROSS_COMPILE to be outside workdir

--- utils.rb
+++ utils.rb
@@ -20,8 +20,8 @@ def cross_compile(name='CROSS_COMPILE')
   if rpi?
     ''
   else
-    ENV['CROSS_COMPILE'] ||= 'tools/arm-bcm2708/arm-bcm2708-linux-gnueabi/bin/arm-bcm2708-linux-gnueabi-'
-    (name ? "#{name}=" : '') + workdir(ENV['CROSS_COMPILE'])
+    ENV['CROSS_COMPILE'] ||= workdir 'tools/arm-bcm2708/arm-bcm2708-linux-gnueabi/bin/arm-bcm2708-linux-gnueabi-'
+    (name ? "#{name}=" : '') + ENV['CROSS_COMPILE']
   end
 end
 
