23eb24455555b962ac9f2b466896ca9223c244ac
notro <notro@tronnes.org>
2014-06-10 17:28:46 +0200

    class Git: several changes

--- git.rb
+++ git.rb
@@ -2,37 +2,59 @@ class Git
   attr_reader :path
   attr_reader :branch
   attr_accessor :verbose
+  attr_accessor :debug
 
-  def initialize(path, branch)
+  def initialize(path, branch=nil)
     @path = File.expand_path path
-    @branch = branch
     @verbose = Git.verbose
+    @debug = false
     
     @last_remote_commit = nil
     raise "not a git repo: #{path}" unless Git.is_repo? path
-    raise "not equipped to handle repo's without a single commit" if empty?
-    create_branch(branch) unless branches.include? branch
-    checkout branch
+    if branch
+      create_branch(branch) unless branches.include? branch
+      checkout branch
+    end
   end
 
-  def git(cmd, talkative=false)
-    puts "cd #{@path} && git #{cmd}" if (talkative || @verbose)
-    res = `cd #{@path} && git #{cmd}`
-    puts res if (talkative || @verbose || $?.to_i != 0)
+  def git(cmd, talkative=nil, print_output=nil)
+    full_cmd = "cd #{@path} && git #{cmd}"
+    puts full_cmd if talkative.nil? ? @verbose : talkative
+    res = `#{full_cmd}`
+    if $?.exitstatus != 0
+      puts res
+      raise "Command failed with status (#{$?.exitstatus}): [#{full_cmd}]"
+    end
+    puts res if print_output
     res
   end
 
   def empty?
     git('log 2>&1 > /dev/null')
-    $?.to_i != 0
+  rescue
+    true
+  else
+    false
+  end
+
+  def branch
+    @branch ||= current_branch
+  end
+
+  def branch=(name)
+    checkout name
+  end
+
+  def branch?(name)
+    branches.include? name
   end
 
   def branches
-    git('branch').gsub('* ', '').strip.split
+    git('branch', @debug, @debug).gsub('* ', '').strip.split
   end
 
-  def current_branch(name)
-    git 'git rev-parse --abbrev-ref HEAD'
+  def current_branch
+    git 'rev-parse --abbrev-ref HEAD'
   end
 
   def create_branch(name)
@@ -44,8 +66,8 @@ class Git
   end
 
   def commit_all(msg)
-    git 'add .'
-    git "commit -a -m \"#{msg}\"", true
+    git 'add .', nil, @debug
+    git "commit -a -m \"#{msg}\"", nil, @debug
   end
 
   def push
@@ -54,26 +76,27 @@ class Git
 
   # no tracked file(s) has changed
   def clean?
-    git('ls-files -m').strip.empty?
+    git('ls-files -m', @debug, @debug).strip.empty?
   end
 
   # no untracked file(s) nor changed tracked file(s)
   def pristine?
-    git('status -s').strip.empty?
+    git('status -s', @debug, @debug).strip.empty?
   end
 
   def last_commit
-    git("log -1 --format=%H").strip
+    git("log -1 --format=%H", nil, @debug).strip
   end
 
   def last_remote_commit
-    @last_remote_commit ||= git("ls-remote origin -h refs/heads/#{branch}").strip.split.first
+    @last_remote_commit ||= {}
+    @last_remote_commit[branch] ||= git("ls-remote origin -h refs/heads/#{branch}", nil, @debug).strip.split.first
   end
 
   def commits_ahead
     remote = last_remote_commit
     return 0 if remote.nil?
-    commits = git "log --format=%H #{branch}"
+    commits = git "log --format=%H #{branch}", nil, @debug
     count = 0
     i = 0
     commits.split("\n").each do |commit|
@@ -87,8 +110,9 @@ class Git
   end
 
   def check
-    raise "#{path} has uncommitted changes" unless clean?
-    puts "WARNING: #{path} has untracked files" unless pristine?
+    raise "not equipped to handle repo's without a single commit" if empty?
+    puts "WARNING: #{path} has changed/untracked files" unless pristine?
+    # FIXME: I don't think this works
     puts "'#{branch}' has not been pushed upstream yet" if last_remote_commit.nil?
     ahead = commits_ahead
     if ahead > 0
